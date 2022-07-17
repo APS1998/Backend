894c98f146f2f545814bb59a421751798d830c64
Amit <1703003@kiit.ac.in>
2022-07-17 18:53:49 +0530

    adding files

---
+++ GroupServer-master/Migrations/20220715174115_MdbConnect06.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupServer.Migrations
+{
+    public partial class MdbConnect06 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
